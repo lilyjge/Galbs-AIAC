e2eefecf9be56db10d369bb770bfecd3350e3be5
BrianZhang1 <brianzhangbz@gmail.com>
2024-11-14 11:12:01 -0500

    move server components under django project directory

--- audio_recognition/transcribe_test.py
+++
@@ -1,8 +0,0 @@
-from audio_recognition.TranscriptionClient import TranscriptionClient
-
-with open("angry_converted.wav", "rb") as f:
-    res = TranscriptionClient(f).transcribe()
-    print("result")
-    print(res)    
-
-
