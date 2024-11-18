460e87f240836c45a0632643f5c73af93d6e2f71
Grace <33011813+gpu314@users.noreply.github.com>
2024-11-18 17:27:25 -0500

    Changed motor response to integer

--- output_generation/main.py
+++ output_generation/main.py
@@ -1,11 +1,12 @@
 '''
-TBD
+A script that takes text input (parsed from audio input) and emotions dictionary (parsed from facial input) to generate audio output, motor angle for arm response, and LED light response as RGB.
 '''
 
+from random import randint
 import numpy as np
 from groq import Groq
 from API_KEY import GROQ_API_KEY
-from BEHAVIOUR import MOTOR, COLOUR
+from BEHAVIOUR import ANGLE_SCALE, MOTOR, COLOUR
 import requests
 from API_KEY import ELEVENLABS_API_KEY
 
@@ -47,7 +48,6 @@ def format_emotions(emotions):
 
 # Generates text reponse using openAI API
 def generate_response(input_text, emotions):
-
     response = client.chat.completions.create(
         model="llama3-8b-8192",
         messages=[
@@ -61,7 +61,6 @@ def generate_response(input_text, emotions):
             }
         ]
     )
-
     return response.choices[0].message.content
 
 
@@ -87,7 +86,7 @@ def tts(voice_id, text, output_file):
 
 # Generate motor control response
 def motor_response(emotions):
-    return MOTOR[emotions["dominant_emotion"]]
+    return int(ANGLE_SCALE*MOTOR[emotions["dominant_emotion"]][0])
 
 
 # Generate LED lights response
@@ -122,7 +121,4 @@ if __name__ == "__main__":
     # Lights
     print("LIGHTS: ")
     print(lights_response(emotions_input))
-
-
-
 
